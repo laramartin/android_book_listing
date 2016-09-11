367c89a3d29531e62aca33a7b204eab9c59507da
Lara Martin <lara.martin.carretero@gmail.com>
2016-09-11 18:22:00 +0200

    Book class

---
+++ app/src/main/java/eu/laramartin/booklisting/Book.java
@@ -0,0 +1,25 @@
+package eu.laramartin.booklisting;
+
+/**
+ * Created by Lara on 11/09/2016.
+ */
+public class Book {
+
+    String author;
+    String title;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+}
